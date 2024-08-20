4cea0bdc7b1cdff63d08b467fe2b36230002dbd2
Casullo <sebastiandanielcasullo@gmail.com>
2024-08-20 18:37:28 -0300

    posicionando widgets

--- MINI PROYECTO/lista comidas.py
+++ MINI PROYECTO/lista comidas.py
@@ -1,10 +1,61 @@
-menu_list = ["cas", "vip", "veg"]
-menu_list[0] = ["Pasta al pesto", "Ensalada César", "Tacos al pastor"]
-menu_list[1] = ["Foie Gras a la plancha con reducción de oporto y chutney de higos", "Ossobuco a la milanesa con risotto de azafrán", "Langosta thermidor con salsa de mantequilla y brandy"]
-menu_list[2] = ["Lentejas estofadas con verduras", "Burrito de frijoles negros", "Pasta con pesto de espinacas"]
-precio_list = ["p_cas", "p_vip", "p_veg"]
-mp_cas = [8000, 6500, 8500]
-mp_vip = [35000, 25000,55000]
-mp_veg = [7500, 6000, 7800]
+# menu_list = ["cas", "vip", "veg"]
+# menu_list[0] = ["Pasta al pesto", "Ensalada César", "Tacos al pastor"]
+# menu_list[1] = ["Foie Gras a la plancha con reducción de oporto y chutney de higos", "Ossobuco a la milanesa con risotto de azafrán", "Langosta thermidor con salsa de mantequilla y brandy"]
+# menu_list[2] = ["Lentejas estofadas con verduras", "Burrito de frijoles negros", "Pasta con pesto de espinacas"]
+# precio_list = ["p_cas", "p_vip", "p_veg"]
+# mp_cas = [8000, 6500, 8500]
+# mp_vip = [35000, 25000,55000]
+# mp_veg = [7500, 6000, 7800]
 # print(menu_list)
 # print(menu_list[0].count("Pasta al pesto"))
+
+menus=[
+{
+    "tipo": "casero",
+    "nombre":"Pasta al pesto",
+    "precio":8000
+},
+{
+    "tipo": "casero",
+    "nombre":"Ensalada César",
+    "precio":6500
+},
+{
+    "tipo": "casero",
+    "nombre":"Tacos al pastor",
+    "precio":8500
+},
+{
+    "tipo": "gourmet",
+    "nombre":"Foie Gras a la plancha con reducción de oporto y chutney de higos",
+    "precio":35000
+},
+{
+    "tipo": "gourmet",
+    "nombre":"Ossobuco a la milanesa con risotto de azafrán",
+    "precio":25000
+},
+{
+    "tipo": "gourmet",
+    "nombre":"Langosta thermidor con salsa de mantequilla y brandy",
+    "precio":55000
+},
+{
+    "tipo": "vegano",
+    "nombre":"Lentejas estofadas con verduras",
+    "precio":7500
+},
+{
+    "tipo": "vegano",
+    "nombre":"Burrito de frijoles negros",
+    "precio":6000
+},
+{
+    "tipo": "vegano",
+    "nombre":"Pasta con pesto de espinacas",
+    "precio":7800
+}
+]
+print(menus[1].get("nombre"))
+
+
--- MINI PROYECTO/practica MP.py
+++ MINI PROYECTO/practica MP.py
@@ -1,24 +1,94 @@
+
 import tkinter as tk
-ventana = tk.Tk()
-ventana.title('Menú desplegable')
-ventana.geometry('400x200')
-barra_menu = tk.Menu(ventana)
-ventana.config(menu=barra_menu)
-#se crean los botones principales de la barra de menu
-menu_principal = tk.Menu(barra_menu)
-menu_principal2 = tk.Menu(barra_menu)
-#Se le asigna la etiqueta a cada boton principal
-barra_menu.add_cascade(label ='Principal', menu=menu_principal)
-barra_menu.add_cascade(label ='Principal2', menu=menu_principal2)
-#Se crean los submenu del menu principal
-submenu = tk.Menu(menu_principal)
-submenu2 = tk.Menu(menu_principal2)
-#Se le asigna la etiqueta al submenu
-menu_principal.add_cascade(label ='Opciones', menu=submenu)
-submenu.add_command(label = 'Opción 1')
-submenu.add_command(label = 'Opción 2')
-#Se le asigna la etiqueta al submenu2
-menu_principal2.add_cascade(label ='Ver', menu=submenu2)
-submenu2.add_command(label = 'Tu')
-submenu2.add_command(label = 'Ano')
-ventana.mainloop()
+# from lista comidas import *
+# main frame
+
+
+
+Raiz = tk.Tk()
+Raiz.title('COMILANDIA')
+Raiz.iconbitmap("comil.ico")
+Raiz.geometry('600x600')
+Raiz.resizable(1,1)
+Raiz.config(bg="green")
+# barra menu
+barra_menu = tk.Menu(Raiz)
+Raiz.config(menu=barra_menu)
+# tres menu en cascada de comidas
+menu_comidas = tk.Menu(barra_menu)
+barra_menu.add_cascade(label ='Menu Comidas', menu=menu_comidas)
+menu_comidas.add_cascade(label= 'Comidas Caseras')
+menu_comidas.add_cascade(label ='Comidas Gourmet')
+menu_comidas.add_cascade(label ='Comidas Vegetarianas')
+
+# etiquetas, entradas y funsion
+etiq1 = tk.Label(Raiz,text= "comida 1",bg="yellow")
+etiq2 = tk.Label(Raiz,text= "comida 2",bg="yellow")
+etiq3 = tk.Label(Raiz,text= "comida 3",bg="yellow")
+
+
+texto1= tk.Entry(Raiz,bg="grey")
+texto2 = tk.Entry(Raiz,bg="grey")
+texto3= tk.Entry(Raiz,bg="grey")
+
+
+
+
+# etiq1.place(x=0.03,y=10,width=1000,height=30)
+# etiq2.place(x=10,y=50,width=1000,height=30)
+# etiq3.place(x=10,y=90, width=1000, height=30)
+
+
+texto1.place(x=120,y=10, width=50,height=30)
+texto2.place(x=120,y=50,width=50,height=30)
+texto3.place(x=120,y=120, width=50)
+
+
+
+
+
+
+etiq1.pack(pady=6)
+etiq2.pack(pady=6)
+etiq3.pack(pady=6)
+texto1.pack(pady=6)
+texto2.pack(pady=6)
+texto3.pack(pady=6)
+# nuevo frame caseras
+# framecomidas=tk.Frame(Raiz, bg="blue")
+# framecomidas.config(width="400", height="300")
+# texto=tk.Label(framecomidas, text="asdasdasdasdasdasd")
+# check comida
+# check = tk.BooleanVar()
+# one=tk.Checkbutton(framecomidas, text="aca iria el diccionario comida casera de la lista comidas", variable=check)
+# one.place(x=10, y=10)
+# framecomidas.pack()
+# one.pack()
+
+# # boton y funcion pedido
+# def pedido():
+#         if one:
+#             return menus[1].get("precio")
+#         elif dos:
+#               return menus[2].get("precio")
+#         else tres:
+#             return menus[3].get("precio")
+
+# boton 
+# def agregar():
+    # if agregar:
+    #     contador = 
+
+
+# boton_pedido=tk.Button(framecomidas, text="Pedir Ya!!", command=pedido)
+# ventana_casera= tk.Tk()
+# ventana_casera.title("asdddd")
+# ventana_casera.geometry ("600x300")
+# ventana_casera.config(bg="blue")
+# onevar = BooleanVar(value=True)
+# one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
+# one.grid(column=0, row=3)
+
+
+Raiz.mainloop()
+
