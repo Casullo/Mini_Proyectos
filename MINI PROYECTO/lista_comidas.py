22c63836ce6bb8a0e8d5f424168fc3eaccbf760e
Casullo <sebastiandanielcasullo@gmail.com>
2024-08-23 15:37:30 -0300

    trabajo

--- MINI PROYECTO/lista_comidas.py
+++ MINI PROYECTO/lista_comidas.py
@@ -1,18 +1,3 @@
-import tkinter as tk
-class menusero:
-    pass
-
-ventana_comida = tk.Frame()
-ventana_comida.config(bg="green")
-etiqueta1 = tk.Label(ventana_comida,text= "comida 1",bg="yellow", font=("Arial", 8)).grid(row=2, column=1)
-texto= tk.Entry(ventana_comida,bg="grey").grid(row=3, column=2)
-etiqueta2 = tk.Label(ventana_comida,text= "comida 2",bg="yellow", font=("Arial", 8)).grid(row=3, column=1)
-texto2 = tk.Entry(ventana_comida,bg="grey").grid(row=3, column=2)
-etiqueta3 = tk.Label(ventana_comida,text= "comida 3",bg="yellow", font=("Arial", 8)).grid(row=4, column=1)
-texto3= tk.Entry(ventana_comida,bg="grey").grid(row=3, column=2)
-
-ventana333=menusero()
-
 menus=[
 {
     "tipo": "casero",
@@ -60,6 +45,28 @@ menus=[
     "precio":7800
 }
 ]
-# print(menus[1].get("nombre"))
+
+# print(menus[1].get("nombre"))    
 
 
+
+
+# self.ventana_comida = tk.Tk()
+# self.ventana_comida.config(bg="green")
+# self.etiqueta1 = tk.Label(ventana_comida,text= "comida 1",bg="yellow", font=("Arial", 8)).grid(row=2, column=1)
+# self.texto1= tk.Entry(ventana_comida,bg="grey").grid(row=3, column=2)
+# self.etiqueta2 = tk.Label(ventana_comida,text= "comida 2",bg="yellow", font=("Arial", 8)).grid(row=3, column=1)
+# self.texto2 = tk.Entry(ventana_comida,bg="grey").grid(row=3, column=2)
+# self.etiqueta3 = tk.Label(ventana_comida,text= "comida 3",bg="yellow", font=("Arial", 8)).grid(row=4, column=1)
+# self.texto3= tk.Entry(ventana_comida,bg="grey").grid(row=3, column=2)
+# self.ventana_comida.pack()
+# self.etiqueta1.pack()
+# self.etiqueta2.pack()
+# self.etiqueta3.pack()
+# self.texto1.pack()
+# self.texto2.pack()
+# self.texto3.pack()
+
+
+
+    
